011200e9aa22bfad92c4b4f3b70b613cc173239e
Saifullah Syahidin <hexters@gmail.com>
2018-03-24 21:43:17 +0700

    async proccess command

--- src/Console/ChekcingTransactionCommand.php
+++ src/Console/ChekcingTransactionCommand.php
@@ -50,28 +50,35 @@ class chekcingTransactionCommand extends Command
         logs::whereIn('status', [0, 1])->where('expired', '>', Carbon::now())
           ->chunk(100, function($transactions){
             foreach($transactions as $trx){
-              $this->info('Payment ID: ' . $trx->payment_id);
-              $check = CoinPayment::api_call('get_tx_info', [
-                'txid' => $trx->payment_id
-              ]);
-              if($check['error'] == 'ok'){
-                $data = $check['result'];
-                if($data['status'] > 0 || $data['status'] < 0){
-                  $trx->update([
-                    'status_text' => $data['status_text'],
-                    'status' => $data['status'],
-                    'confirmation_at' => ((INT) $data['status'] === 100) ? date('Y-m-d H:i:s', $data['time_completed']) : null
-                  ]);
+              $this->async_proccess($trx, function($check){
 
-                  $this->info('Status : ' . $data['status_text']);
+                if($check['error'] == 'ok'){
+                  $data = $check['result'];
+                  $this->info('Address: ' . $data['payment_address']);
+                  if($data['status'] > 0 || $data['status'] < 0){
+                    $trx->update([
+                      'status_text' => $data['status_text'],
+                      'status' => $data['status'],
+                      'confirmation_at' => ((INT) $data['status'] === 100) ? date('Y-m-d H:i:s', $data['time_completed']) : null
+                    ]);
 
-                  // Send hook
-                  $this->info('======================= SENDING WEBHOOK =======================');
-                  $data['request_type'] = 'schedule_transaction';
-                  dispatch(new webhookProccessJob($data));
+                    $this->info('Status : ' . $data['status_text']);
+
+                    // Send hook
+                    $this->info('======================= SENDING WEBHOOK =======================');
+                    $data['request_type'] = 'schedule_transaction';
+                    dispatch(new webhookProccessJob($data));
+                  }
                 }
-              }
+              });
             }
           });
+    }
+
+    private function async_proccess($trx, $callback){
+      $check = CoinPayment::api_call('get_tx_info', [
+        'txid' => $trx->payment_id
+      ]);
+      return $callback($check);
     }
 }
