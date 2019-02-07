2d4e21a23d7d538c64cebe11122c92f6bcc7a06a
saifullah <hexters@gmail.com>
2019-02-08 06:04:57 +0700

    Closed #18 Fix bug division by zero

--- src/Http/Controllers/CoinPaymentController.php
+++ src/Http/Controllers/CoinPaymentController.php
@@ -41,11 +41,9 @@ class CoinPaymentController extends Controller {
       $fiat = [];
       $coins_accept = [];
       foreach($rates as $i => $coin){
-          if ($rates[$i]['status'] !== 'online') continue;
-          if ($rates[$i]['rate_btc'] == 0) continue;
 
         if((INT) $coin['is_fiat'] === 0){
-          $rate = ($rateAmount / $rates[$i]['rate_btc']);
+          $rate = $rates[$i]['rate_btc'] === 0 ? 0 : ($rateAmount / $rates[$i]['rate_btc']);
           $coins[] = [
             'name' => $coin['name'],
             'rate' => number_format($rate,8,'.',''),
@@ -59,7 +57,7 @@ class CoinPaymentController extends Controller {
         }
 
         if((INT) $coin['is_fiat'] === 0 && $coin['accepted'] == 1){
-          $rate = ($rateAmount / $rates[$i]['rate_btc']);
+          $rate = $rates[$i]['rate_btc'] === 0 ? 0 : ($rateAmount / $rates[$i]['rate_btc']);
           $coins_accept[] = [
             'name' => $coin['name'],
             'rate' => number_format($rate,8,'.',''),
